da4f84d7f9a5c1cbfaf681be8e8f24bf83e070a7
José Eduardo <limaedu18@hotmail.com>
2024-11-19 18:58:40 -0300

    First commit

---
+++ src/main/java/com/jedulima/mybooklist/MybooklistApplication.java
@@ -0,0 +1,13 @@
+package com.jedulima.mybooklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybooklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybooklistApplication.class, args);
+	}
+
+}
